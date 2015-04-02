48c31a4c5c4c123b5b14bac2c45299dc3c52d212
Robin Choudhury <xrobinc@gmail.com>
2015-04-02 14:22:06 -0700

    example: Use code from getting started guide.

--- example/led.c
+++ example/led.c
@@ -2,21 +2,20 @@
 #include <util/delay.h>
 
 enum {
- BLINK_DELAY_MS = 1000,
+ BLINK_DELAY_MS = 50,
 };
 
-int main (void)
-{
+int main (void) {
  /* set pin 5 of PORTB for output*/
- DDRB |= _BV(DDB5);
+ DDRB |= (1<<PB5);
 
  while(1) {
   /* set pin 5 high to turn led on */
-  PORTB |= _BV(PORTB5);
+  PORTB |= (1<<PB5);
   _delay_ms(BLINK_DELAY_MS);
 
   /* set pin 5 low to turn led off */
-  PORTB &= ~_BV(PORTB5);
+  PORTB &= ~(1<<PB5);
   _delay_ms(BLINK_DELAY_MS);
  }
 
