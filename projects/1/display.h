fe6ad8cffd87295ecc6dea2d0cfed950f02d8297
Robin Choudhury <xrobinc@gmail.com>
2015-04-15 22:45:39 -0700

    project1: Move #defines to the write headers.

--- projects/1/display.h
+++ projects/1/display.h
@@ -1,7 +1,11 @@
 #ifndef DISPLAY_H
 #define DISPLAY_H
 
+// Button port on Port B
+#define BUTTON_CHECK 3
 #define BUTTON_PRESSED (PINB & (1 << BUTTON_CHECK))
+#define DEFAULT_MESSAGE "Hello World"
+#define ALT_MESSAGE "Project 1 done!"
 
 static void clear_display(char firstTime);
 void one_shot();
--- projects/1/message.h
+++ projects/1/message.h
@@ -1,11 +1,6 @@
 #ifndef MESSAGE_H
 #define MESSAGE_H
 
-// Button port on Port B
-#define BUTTON_CHECK 3
-#define DEFAULT_MESSAGE "Hello World"
-#define ALT_MESSAGE "Project 1 done!"
-
 extern const char NIBBLE_MODE;
 extern const char TOGGLE;
 
