8ee81aa7ffbb40a0fd60da6c0f4c2d2b41fdc22a
Robin Choudhury <xrobinc@gmail.com>
2015-06-05 14:22:08 -0700

    assn: Complete interrupt tutorial.

--- assn/external-interrupts/interrupt.c
+++ assn/external-interrupts/interrupt.c
@@ -12,21 +12,21 @@ int main(void) {
    sei();    //enable global interrupts
    while(1); //stuck here forever :)
    return 0;
-
-} // end main
-
-void initPCINT0(void) {
-   PCICR = 0x01;     // enable PCINT7..0
-   PCMSK0 = 0x01;    // enable PCINT0 interrupt
-   PCIFR = 0x01;     // clear previous interrupts
 }
 
+void initPCINT0(void) {
+   //PCICR = 0x01;     // Enable PCINT7..0
+   //PCMSK0 = 0x01;    // Enable PCINT0 interrupt
+   //PCIFR = 0x01;     // Clear previous interrupts
+   EIMSK = 0x01;     // Enable PCINT0
+   EICRA = 0x01;     // Any voltage change will cause an interrupt.
+   EIFR = 0x01;      // Clear previous interrupts. Set it.
+}
 
-ISR(PCINT0_vect) {
+ISR(INT0_vect) {
    cli();
    PORTB |= 1<<PB5;
    _delay_ms(250);
    PORTB &= ~(1<<PB5);
    sei();
 }
-
